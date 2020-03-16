3ca494ac61a84c0f17d5f2c13327f40222c5191e
dhruvs009 <dhruv18230@iiitd.ac.in>
2020-03-17 01:04:50 +0530

    Updates

---
+++ precache-manifest.4b886e315e5080816217052b9d20a72e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2501c63f5e9ea69aeff7c4c2a1ac8d0",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "d52f4fd8ac23149e9d1e",
+    "url": "/me/static/css/2.f681e662.chunk.css"
+  },
+  {
+    "revision": "09cc96ee06810c6ac57e",
+    "url": "/me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d52f4fd8ac23149e9d1e",
+    "url": "/me/static/js/2.43931406.chunk.js"
+  },
+  {
+    "revision": "ba1665840a4211d22917503a2a5544f6",
+    "url": "/me/static/js/2.43931406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09cc96ee06810c6ac57e",
+    "url": "/me/static/js/main.e879d69f.chunk.js"
+  },
+  {
+    "revision": "59de9076f02f5d974473",
+    "url": "/me/static/js/runtime-main.38d0503b.js"
+  },
+  {
+    "revision": "452fd07b4be681cd330317cc5c16cd0a",
+    "url": "/me/static/media/ATTRACT.452fd07b.png"
+  },
+  {
+    "revision": "0d42cfc1bf4ad123d9b930d5ef165f12",
+    "url": "/me/static/media/GlideIT.0d42cfc1.jpeg"
+  },
+  {
+    "revision": "b933af1153359698dd2f220ddea6706d",
+    "url": "/me/static/media/PlantsVSZombies.b933af11.png"
+  },
+  {
+    "revision": "36e37eef36c71079d36653bc06073b8e",
+    "url": "/me/static/media/iiitd.36e37eef.png"
+  },
+  {
+    "revision": "f7ae7d5efc05a508d2a831b4a2a5c04d",
+    "url": "/me/static/media/weave.f7ae7d5e.png"
+  },
+  {
+    "revision": "4e7dd48afa9fe50f02a96a6137a70c51",
+    "url": "/me/static/media/wellowise.4e7dd48a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.bee93cf9166301d4c5db4fb1a2289a67.js"
+  "/me/precache-manifest.4b886e315e5080816217052b9d20a72e.js"
 );
 
 self.addEventListener('message', (event) => {
